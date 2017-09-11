a6291de48a8b8a70b7494a8d3021687b56994bf7
aartha_mohit <mohitmaharjan7@gmail.com>
2017-09-11 17:22:20 +0545

    call the remote web api

--- extension.js
+++ extension.js
@@ -1,7 +1,7 @@
 
 
 $(function(){
-  var data = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]; // the ids coming back from serviceA
+  var data = [1,2,3,4]; // the ids coming back from serviceA
   // 2. Declare an array of Deferred objects
   var processItemsDeferred = [];
   // 3. For each element of data, create a Deferred push push it to the array
@@ -16,22 +16,29 @@ $(function(){
 function processItem(data) {
   // 3.1.1. Create the Deferred object and output some debug
   var dfd = $.Deferred();
+  var theUrl = 'https://jsonplaceholder.typicode.com/comments';
   console.log('called processItem' +  data);
+  var xmlHttp = new XMLHttpRequest();
+   xmlHttp.onreadystatechange = function() {
+       if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+           var element  = document.createElement("p");
+           element.innerText = xmlHttp.responseText;
+           var bodyElement = document.getElementById('console');
+           bodyElement.appendChild(element);
+           dfd.resolve();
+         }
+   }
+   xmlHttp.open("GET", theUrl, true); // true for asynchronous
+   xmlHttp.send(null);
   // 3.1.2. After some timeout, resolve the current Deferred
   //in the real world, this would probably make an AJAX call.
   //setTimeout(function() { dfd.resolve() }, 4000);
   // 3.1.3. Return that Deferred (to be inserted into the array)
-  var element  = document.createElement("li");
-  element.innerText = "Completed processing data " + data;
-  var bodyElement = document.getElementById('console');
-  bodyElement.appendChild(element);
-  dfd.resolve();
   return dfd.promise();
 }
 // 4.1. Function called when all deferred are resolved
 function everythingDone(){
   // 4.1.1. Do some debug trace
-  console.log('processed all items');
   var element = document.createElement("p");
   element.innerText = "Finally completed all processes...";
   var bodyElement = document.getElementById('console');
